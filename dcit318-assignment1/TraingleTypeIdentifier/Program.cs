f038dc12899bf21445f816ae7e89783ca360974d
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-07-29 20:50:15 +0000

    Triangle Identifier Application

---
+++ dcit318-assignment1/TraingleTypeIdentifier/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TraingleTypeIdentifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
