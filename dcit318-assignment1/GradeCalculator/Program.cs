b0b998e07f8f8607b3eb3c3155fc51fab8415c7d
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-07-29 20:42:17 +0000

    Grade calculator completed

---
+++ dcit318-assignment1/GradeCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace GradeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a numerical grade between 0 and 100");
+                    string input = Console.ReadLine();
+
+                    grade = Convert.ToInt32(input);
+
+                    if (grade < 0 || grade > 100)
+                    {
+                        Console.WriteLine("Please enter a number between 0 and 100....");
+                        continue;
+                    }
+
+                    if (grade >= 90)
+                    {
+                        Console.WriteLine("Your grade is A");
+                    }
+                    else if (grade >= 80)
+                    {
+                        Console.WriteLine("Your grade is B");
+                    }
+                    else if (grade >= 70)
+                    {
+                        Console.WriteLine("Your grade is  C");
+                    }
+                    else if (grade >= 60)
+                    {
+                        Console.WriteLine("Your grade is D");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your grade is F");
+                    }
+
+                    break;
+
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine("Please enter a number.....");
+                    Console.WriteLine(fe.Message);
+                }
+            }
+        }
+    }
+}
---
+++ dcit318-assignment1/dcit318-assignment1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dcit318_assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
