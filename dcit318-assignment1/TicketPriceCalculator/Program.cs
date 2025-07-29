3ab7387e853cdf038b28d17fc24ce5f72690c10f
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-07-29 20:44:53 +0000

    TicketPriceCalculator completed

---
+++ dcit318-assignment1/TicketPriceCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace TicketPriceCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ticketPrice = 10;
+            int disount = 7;
+            int age;
+            int discountAmount;
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Please enter age: ");
+                    string input = Console.ReadLine();
+
+                    age = Convert.ToInt32(input);
+
+                    if (age <= 12 || age >= 65)
+                    {
+                        discountAmount = ticketPrice - disount;
+                        Console.WriteLine("Your discounted price is Ghc" + discountAmount);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your ticket price is Ghc " + ticketPrice);
+                    }
+
+                    break;
+                }
+
+
+
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Age must be a number.....");
+                Console.WriteLine(fe.Message);
+            }
+        }
+    }
+}
